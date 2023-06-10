15bd5b011a0f5111bedb5a9e51183ec47c81d7b3
DevKevINTender <89483884+DevKevINTender@users.noreply.github.com>
2023-06-10 21:23:17 +0300

    Update WeaponDataSerivce.cs

--- WeaponDataSerivce.cs
+++ WeaponDataSerivce.cs
@@ -5,49 +5,22 @@ using UnityEngine;
 
 namespace Data.SaveLoadPlayerProgress
 {
-    public class SaveLoad : ISaveLoad
+    public class WeaponDataSerivce : IDataService
     {
-        private readonly ISavedProgressData _progress;
-        private readonly List<ISaveLoadProgress<PlayerProgress>> _savedPlayerProgresses;
-        private readonly List<ISaveLoadProgress<WeaponProgress>> _savedWeaponProgresses;
+        private readonly WeaponProgress _savedWeaponProgresses;
 
-        private const string PlayerProgressKey = "PlayerProgress";
         private const string WeaponProgressKey = "WeaponProgress";
         
-        public SaveLoad(ISavedProgressData progress, List<ISaveLoadProgress<PlayerProgress>> savedPlayerPlayerProgresses, List<ISaveLoadProgress<WeaponProgress>> savedWeaponProgresses)
+        public WeaponDataSerivce(WeaponProgress savedWeaponProgresses)
         {
-            _progress = progress;
-            _savedPlayerProgresses = savedPlayerPlayerProgresses;
             _savedWeaponProgresses = savedWeaponProgresses;
         }
 
-        public void SaveProgress()
+        public void SaveWeaponProgress()
         {
-            if (_savedPlayerProgresses != null)
-            {
-                for (int i = 0; i < _savedPlayerProgresses.Count; i++)
-                {
-                    _savedPlayerProgresses[i].UpdateProgress(_progress.PlayerProgress);
-                }
-            }
-            
-            if (_savedWeaponProgresses != null)
-            {
-                for (int i = 0; i < _savedWeaponProgresses.Count; i++)
-                {
-                    _savedWeaponProgresses[i].UpdateProgress(_progress.WeaponProgress);
-                }
-            }
-
-            PlayerPrefs.SetString(PlayerProgressKey, _progress.PlayerProgress.ToJson());
-            PlayerPrefs.SetString(WeaponProgressKey, _progress.WeaponProgress.ToJson());
+     
         }
-
-        public PlayerProgress LoadPlayerProgress() =>
-            PlayerPrefs
-                .GetString(PlayerProgressKey)
-                .ToDeserialized<PlayerProgress>();
-
+        
         public WeaponProgress LoadWeaponProgress() =>
             PlayerPrefs
                 .GetString(WeaponProgressKey)
